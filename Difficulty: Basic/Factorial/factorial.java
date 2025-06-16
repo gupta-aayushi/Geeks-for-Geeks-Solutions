d50c5ee822076cff3e25a55271f119c947d5311b
AAYUSHI GUPTA <134951518+gupta-aayushi@users.noreply.github.com>
2025-06-16 21:25:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,28 +1,13 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            int N = Integer.parseInt(read.readLine());
-            Solution ob = new Solution();
-            System.out.println(ob.factorial(N));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
+// User function Template for Java
 class Solution {
-    static int factorial(int n) {
-        if (n==1 || n==0){
-            return 1;
+    public static int nFactorial(int n) {
+        // Write your code here
+        if (n==1||n==0) return 1;
+        int arr[] = new int[n+1];
+        arr[1] = 1;
+        for (int i=2; i<=n; i++){
+            arr[i] = i*arr[i-1];
         }
-        return n*factorial(n-1);
+        return arr[n];
     }
 }
